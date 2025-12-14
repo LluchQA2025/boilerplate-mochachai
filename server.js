196673f67808d5844a236b2073751ed041e25733
LluchQA2025 <lluchfilms@gmail.com>
2025-12-13 22:56:44 -0300

    Complete FCC chai-http PUT challenge

--- server.js
+++ server.js
@@ -2,116 +2,45 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 
 const app = express();
 
-// ====== FCC boilerplate: assertion analyser (necesario para /_api/get-tests) ======
-const analyser = require('./assertion-analyser');
-
-// ========= CORS (para que FCC pueda llamar endpoints) =========
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(204);
-  }
-  next();
-});
-
-// ========= MIDDLEWARES =========
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// ========= HELPERS =========
-function explorerNameBySurname(surname) {
-  if (surname === 'Colombo') return 'Cristoforo';
-  if (surname === 'Vespucci') return 'Amerigo';
-  if (surname === 'da Verrazzano') return 'Giovanni';
-  return '';
-}
+app.use(express.static('public'));
 
-function renderPage(name = '', surname = '') {
-  return `
-    <!doctype html>
-    <html>
-      <head>
-        <meta charset="utf-8" />
-        <title>Famous Italian Explorers</title>
-      </head>
-      <body>
-        <h1>Famous Italian Explorers</h1>
-
-        <form action="/travellers" method="POST">
-          <label for="surname">Surname</label>
-          <input id="surname" name="surname" type="text" />
-          <input id="submit" name="submit" type="submit" value="submit" />
-        </form>
-
-        <hr />
-
-        <div id="result">
-          <span id="name">${name}</span>
-          <span id="surname">${surname}</span>
-        </div>
-      </body>
-    </html>
-  `;
-}
-
-// ========= FCC REQUIRED ROUTE =========
-// freeCodeCamp llama esto para obtener los tests a ejecutar
-app.get('/_api/get-tests', (req, res) => {
-  res.json(analyser.getTests());
-});
-
-// ========= ROUTES =========
-app.get('/', (req, res) => {
-  res.status(200).type('html').send(renderPage());
-});
-
-app.get('/hello', (req, res) => {
+// GET /hello
+app.get('/hello', function (req, res) {
   const name = req.query.name || 'Guest';
-  res.type('text').send('hello ' + name);
+  res.send(`hello ${name}`);
 });
 
-// ===== FCC CHALLENGE (chai-http PUT) =====
-app.put('/travellers', (req, res) => {
+// PUT /travellers  ✅ ESTE ES EL CLAVE
+app.put('/travellers', function (req, res) {
   const surname = req.body.surname;
-  const name = explorerNameBySurname(surname);
 
-  res.status(200).json({
-    name,
-    surname
+  let name = '';
+
+  if (surname === 'Colombo') {
+    name = 'Cristoforo';
+  } else if (surname === 'Vespucci') {
+    name = 'Amerigo';
+  } else if (surname === 'da Verrazzano') {
+    name = 'Giovanni';
+  }
+
+  res.json({
+    name: name,
+    surname: surname
   });
 });
 
-// ===== Para el FORM (Zombie.js) =====
-app.post('/travellers', (req, res) => {
-  const surname = req.body.surname;
-  const name = explorerNameBySurname(surname);
-
-  res.status(200).type('html').send(renderPage(name, surname));
+// Start server
+const listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Listening on port ' + listener.address().port);
 });
-
-// ========= START SERVER =========
-const port = process.env.PORT || 3000;
-
-if (!module.parent) {
-  app.listen(port, () => {
-    console.log('Listening on port ' + port);
-
-    try {
-      const runner = require('./test-runner');
-      runner.run();
-    } catch (e) {
-      // OK
-    }
-  });
-}
 
 module.exports = app;
