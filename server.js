0c7a0631db9931d37e25967bf5a26b8be5eb64c3
LluchQA2025 <lluchfilms@gmail.com>
2025-12-16 14:54:55 -0300

    fix: corrige PUT /travellers para pasar tests FCC

--- server.js
+++ server.js
@@ -4,58 +4,45 @@ const path = require('path');
 
 const app = express();
 
-// Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Datos de prueba
 const explorers = {
-  colombo: {
+  'colombo': {
     name: 'Cristoforo',
     surname: 'Colombo',
     dates: '1451 - 1506'
   },
-  vespucci: {
+  'da verrazzano': {
+    name: 'Giovanni',
+    surname: 'da Verrazzano',
+    dates: '1485 - 1528'
+  },
+  'vespucci': {
     name: 'Amerigo',
     surname: 'Vespucci',
     dates: '1454 - 1512'
-  },
-  verranzano: {
-    name: 'Giovanni',
-    surname: 'Verrazzano',
-    dates: '1485 - 1528'
   }
 };
 
-// Ruta GET para página principal
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
-// Ruta POST para formulario
-app.post('/travellers', (req, res) => {
-  const surname = req.body.surname?.toLowerCase();
-  const data = explorers[surname];
-  if (!data) {
-    return res.status(404).send('Not Found');
-  }
-  res.json(data);
-});
-
-// ✅ Ruta PUT requerida por el challenge
 app.put('/travellers', (req, res) => {
-  const surname = req.body.surname?.toLowerCase();
-  const data = explorers[surname];
-  if (!data) {
+  const surname = req.body.surname;
+  const key = surname.toLowerCase();
+  const traveller = explorers[key];
+
+  if (!traveller) {
     return res.status(404).json({ error: 'Not Found' });
   }
-  res.status(200).json(data);
+
+  res.json(traveller);
 });
 
-// Escuchar en el puerto adecuado para Render
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log(`Servidor escuchando en puerto ${listener.address().port}`);
 });
 
-// Exportar app para los tests
 module.exports = app;
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -3,12 +3,10 @@ const chaiHttp = require('chai-http');
 const server = require('../server');
 
 const assert = chai.assert;
-
 chai.use(chaiHttp);
 
-suite('Functional Tests', function () {
-
-  test('Send {surname: "Colombo"}', function (done) {
+describe('Functional Tests', function () {
+  it('Send {surname: "Colombo"}', function (done) {
     chai
       .request(server)
       .put('/travellers')
@@ -22,4 +20,17 @@ suite('Functional Tests', function () {
       });
   });
 
+  it('Send {surname: "da Verrazzano"}', function (done) {
+    chai
+      .request(server)
+      .put('/travellers')
+      .send({ surname: 'da Verrazzano' })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'application/json');
+        assert.equal(res.body.name, 'Giovanni');
+        assert.equal(res.body.surname, 'da Verrazzano');
+        done();
+      });
+  });
 });
